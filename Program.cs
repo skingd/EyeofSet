bdd0515967f1aee78a2bc08c9794e0a12bf3d7b4
Stephen King <skingdjr@outlook.com>
2023-12-29 03:16:56 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections;
+using System.IO
+// load remote db with path without exposing the path
+// https://stackoverflow.com/questions/38767976/how-to-load-sqlite-database-from-a-remote-server-in-c-sharp
+
+
+
+
+var dbPath = Environment.GetEnvironmentVariable("ConanGame");
+Console.WriteLine($"dbPath: {dbPath.Value}");
+namespace SQLiteSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection("" +
+                new SqliteConnectionStringBuilder
+                {
+                    DataSource = "sample.db"
+                }))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT 1";
+
+                var result = command.ExecuteScalar();
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
+
